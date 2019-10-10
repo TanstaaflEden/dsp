3277c7c4a6921c83b9c18b733c007cf148441014
TanstaaflEden <benjaminhartnett@gmail.com>
2019-10-10 15:34:58 -0500

    added lua

--- scripts/zones/Waughroon_Shrine/bcnms/crustacean_conundrum.lua
+++ scripts/zones/Waughroon_Shrine/bcnms/crustacean_conundrum.lua
@@ -4,11 +4,36 @@
 -- BCNM20
 -----------------------------------
 
+require("scripts/globals/titles");
+require("scripts/globals/quests");
+
+-----------------------------------
+-- EXAMPLE SCRIPT
+--
+-- What should go here:
+-- giving key items, playing ENDING cutscenes
+--
+-- What should NOT go here:
+-- Handling of "battlefield" status, spawning of monsters,
+-- putting loot into treasure pool,
+-- enforcing ANY rules (SJ/number of people/etc), moving
+-- chars around, playing entrance CSes (entrance CSes go in bcnm.lua)
+
+-- After registering the BCNM via bcnmRegister(bcnmid)
 function onBcnmRegister(player,instance)
 end;
 
+-- Physically entering the BCNM via bcnmEnter(bcnmid)
 function onBcnmEnter(player,instance)
 end;
+
+-- Leaving the BCNM by every mean possible, given by the LeaveCode
+-- 1=Select Exit on circle
+-- 2=Winning the BC
+-- 3=Disconnected or warped out
+-- 4=Losing the BC
+-- via bcnmLeave(1) or bcnmLeave(2). LeaveCodes 3 and 4 are called
+-- from the core when a player disconnects or the time limit is up, etc
 
 function onBcnmLeave(player,instance,leavecode)
     if (leavecode == 2) then -- play end CS. Need time and battle id for record keeping + storage
---
+++ scripts/zones/Waughroon_Shrine/mobs/Metal_Crab.lua
@@ -0,0 +1,83 @@
+-----------------------------------
+-- Area: Waughroon Shrine
+-- MOB: Metal Crab
+-- BCNM Fight: Crustacean Conundrum
+-----------------------------------
+mixins = {require("scripts/mixins/job_special")};
+
+function onMobSpawn(mob)
+
+	--A combination of mods to bring all damage dealt into the 0-2 range per hit
+
+	mob:addMod(dsp.mod.DMGPHYS,-67)
+	mob:addMod(dsp.mod.DMGBREATH,-92)
+	mob:addMod(dsp.mod.UDMGPHYS,0)
+	mob:addMod(dsp.mod.UDMGBREATH,-92)
+	mob:addMod(dsp.mod.UDMGMAGIC,-80)
+	
+	mob:setMod(dsp.mod.VIT,300)
+    mob:setMod(dsp.mod.DEF,3000)
+	mob:setMod(dsp.mod.RDEF,3000)
+	mob:setMod(dsp.mod.CRIT_DEF_BONUS,50)
+	
+	mob:setMod(dsp.mod.MDEF,1000)
+end
+
+function onMobFight(mob, target)
+	local uDamagePhysical = 0
+	
+	--Stronger weapons get a larger reduction in order to bring everyone down to 0-2 DMG per hit
+	
+	local uDamagePhysicalW = (target:getWeaponDmg() - 8) * -5
+	local uDamagePhysicalS = (target:getOffhandDmg() - 8) * -5
+	
+	--Finds the largest reduction to apply to UDMGPHYS
+
+	if (uDamagePhysical > uDamagePhysicalW) then
+		uDamagePhysical = uDamagePhysicalW
+	end
+	
+	if (uDamagePhysical > uDamagePhysicalS) then
+		uDamagePhysical = uDamagePhysicalS
+	end
+	
+	--Caps the reduction to UDMGPHYS at -75
+	
+	if (uDamagePhysical < -75) then
+		uDamagePhysical = -75
+	end
+	
+	mob:setMod(dsp.mod.UDMGPHYS,uDamagePhysical)
+	
+	target:setMod(dsp.mod.BP_DAMAGE,-95)
+	target:setMod(dsp.mod.SHIELD_BASH,-20)
+	target:setMod(dsp.mod.WEAPON_BASH,-6)
+	target:setMod(dsp.mod.SPIKES_DMG,0)
+end
+
+--The crabs have an Endrain effect that goes off 50% of the time for 30 HP
+
+function onAdditionalEffect(mob,target,damage)
+    local chance = 50
+
+    if (math.random(0,99) >= chance) then
+        return 0,0,0
+    else
+        local power = 30
+        
+        power = power * applyResistanceAddEffect(mob,target,dsp.magic.ele.DARK,0)
+        power = adjustForTarget(target,power,dsp.magic.ele.DARK)
+        power = finalMagicNonSpellAdjustments(mob,target,dsp.magic.ele.DARK,power)
+
+        if (power < 0) then
+            power = 0
+        else
+            mob:addHP(power)
+        end
+
+        return dsp.subEffect.HP_DRAIN, dsp.msg.basic.ADD_EFFECT_HP_DRAIN, power
+    end
+end
+
+function onMobDeath(mob, player, isKiller)
+end;
